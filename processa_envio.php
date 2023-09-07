05048d543f9e7d3e590d9b2235623d1d4e4f38a2
Kerven Kildhery <kervensilvva@gmail.com>
2023-09-07 15:03:50 -0300

    criado a classe mensagem e instanciado um objeto; feito também a regra de mensagem caso a mensagem é valida ou não a partir dos campos vazio do formulario

--- processa_envio.php
+++ processa_envio.php
@@ -1,5 +1,46 @@
 <?php
 
-print_r($_POST);
+//print_r($_POST);
+
+class Mensagem
+{
+    private $para = null;
+    private $assunto = null;
+    private $mensagem = null;
+
+    public function __get($attr)
+    {
+        return $this->$attr;
+    }
+
+    public function __set($attr, $valor)
+    {
+        $this->$attr = $valor;
+    }
+
+    public function mensagemValida()
+    {
+        if (empty($this->para) || empty($this->assunto) || empty($this->mensagem)) {
+            return false;
+        }
+        return true;
+    }
+}
+
+$mensagem = new Mensagem();
+
+$mensagem->__set('para', $_POST['para']);
+$mensagem->__set('assunto', $_POST['assunto']);
+$mensagem->__set('mensagem', $_POST['mensagem']);
+
+//echo '<pre>';
+//print_r($mensagem);
+//echo '</pre>';
+
+if ($mensagem->mensagemValida()) {
+    echo 'Mensagem é valida';
+} else {
+    echo 'Mensagem invalida';
+}
 
 ?>
